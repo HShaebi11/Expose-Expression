cbaee162116cd4eab3bc32e7832e40f91cb13ecc
HShaebi11 <hshaebi.11@gmail.com>
2024-12-25 05:54:41 +0000

    colour input

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let colourBG = 220; // Default background color
+
 function setup() {
   // Get the output div element
   let outputDiv = document.getElementById('output');
@@ -11,12 +13,22 @@ function setup() {
   // Make sure the canvas style is set to fill the container
   canvas.style('width', '100%');
   canvas.style('height', '100%');
+
+  // Get the HTML color picker element and set up event listener
+  let colourPicker = document.getElementById('colourBG');
+  colourPicker.value = '#DCDCDC'; // Set initial value
+  colourPicker.style.backgroundColor = colourPicker.value; // Set initial background color
+  
+  colourPicker.addEventListener('input', function() {
+    colourBG = color(this.value); // Convert HTML color to p5.js color
+    this.style.backgroundColor = this.value; // Update the input element's background color
+  });
 }
 
 function draw() {
-  background(220);
-  fill(255, 0, 0);  // Set fill color to red (RGB values)
-  circle(width/2, height/2, 100);  // Draw circle in center of canvas, diameter 100
+  background(colourBG);
+  fill(255, 0, 0);
+  circle(width/2, height/2, 100);
 }
 
 // Resize canvas when window is resized
