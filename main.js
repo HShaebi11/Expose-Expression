30574e1d436fa094a60a69799493cd8853f19974
HShaebi11 <hshaebi.11@gmail.com>
2024-12-27 03:00:27 +0000

    sliders work

--- main.js
+++ main.js
@@ -2,24 +2,33 @@ let font;
 let pg;
 let textE1 = "666";
 
-let tX,tY,sp,dspx,dspy,fct;
+let E1TX = 20;      // Number of tiles in X direction
+let E1TY = 20;      // Number of tiles in Y direction
+let E1Speed = 0.02; // Speed of the wave animation
+let E1DX = 0.1;     // Wave density X
+let E1DY = 0.1;     // Wave density Y
+let E1Offset = 20;  // How far the tiles move
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('output');
-  createSliders();
   pg = createGraphics(windowWidth, windowHeight);
 }
 
 function draw() {
+  // Update variables from sliders
+  E1TX = document.getElementById('rangeE1TX').value;
+  E1TY = document.getElementById('rangeE1TY').value;
+  E1DX = document.getElementById('rangeE1DX').value;
+  E1DY = document.getElementById('rangeE1DY').value;
+  E1Offset = document.getElementById('rangeE1Offset').value;
+  E1Speed = document.getElementById('rangeE1Speed').value;
+
   background(0);
 
-
   // PGraphics
-
-  pg.background(0);
+  pg.background(0, 0);
   pg.fill(255);
-  // pg.textFont(font);
   pg.textSize(400);
   pg.push();
   pg.translate(width/2, height/2);
@@ -27,77 +36,48 @@ function draw() {
   pg.text(textE1, 0, 0);
   pg.pop();
 
+  let E1tilesX = E1TX;
+  let E1tilesY = E1TY;
 
-  let tilesX = tX.value();
-  let tilesY = tY.value();
+  let E1tileW = int(width/E1tilesX);
+  let E1tileH = int(height/E1tilesY);
 
-  let tileW = int(width/tilesX);
-  let tileH = int(height/tilesY);
-
-  for (let y = 0; y < tilesY; y++) {
-    for (let x = 0; x < tilesX; x++) {
+  for (let y = 0; y < E1tilesY; y++) {
+    for (let x = 0; x < E1tilesX; x++) {
 
       // WARP
-      let waveX = int(sin(frameCount * sp.value() + ( x * y ) * dspx.value()) * fct.value());
-      let waveY = int(sin(frameCount * sp.value() + ( x * y ) * dspy.value()) * fct.value());
+      let E1waveX = int(sin(frameCount * E1Speed + (x * y) * E1DX) * E1Offset);
+      let E1waveY = int(sin(frameCount * E1Speed + (x * y) * E1DY) * E1Offset);
 
-      if (dspx.value() === 0){
-          waveX = 0;
-      }
-
-      if (dspy.value() === 0){
-          waveY = 0;
-      }
+      // Remove or comment these out
+      // if (0 === 0){
+      //     E1waveX = 0;
+      // }
+      // if (0 === 0){
+      //     E1waveY = 0;
+      // }
       
       // image(pg,0,0)
       
       // SOURCE
-      let sx = x*tileW + waveX;
-      let sy = y*tileH + waveY;
-      let sw = tileW;
-      let sh = tileH;
+      let E1sx = x*E1tileW + E1waveX;
+      let E1sy = y*E1tileH + E1waveY;
+      let E1sw = E1tileW;
+      let E1sh = E1tileH;
 
 
       // DESTINATION
-      let dx = x*tileW;
-      let dy = y*tileH;
-      let dw = tileW;
-      let dh = tileH;
+      let E1dx = x*E1tileW;
+      let E1dy = y*E1tileH;
+      let E1dw = E1tileW;
+      let E1dh = E1tileH;
 
 
 
-      copy(pg, sx, sy, sw, sh, dx, dy, dw, dh);
+      copy(pg, E1sx, E1sy, E1sw, E1sh, E1dx, E1dy, E1dw, E1dh);
 
     }
   }
-}
-
-function createSliders(){
-  tX = createSlider(1, 80, 16, 1);
-  tX.position(20, height + 40);
-  createP('Tiles X').position(20, height);
-
-  tY = createSlider(1, 80, 16, 1);
-  tY.position(20, height + 100);
-  createP('Tiles Y').position(20, height+60);
-
-  sp = createSlider(0, 1, 0.005, 0.01);
-  sp.position(20, height + 160);
-  createP('Speed').position(20, height+120);
-
-  dspx = createSlider(0, 0.1, 0.05, 0.001);
-  dspx.position(180, height + 40);
-  createP('Displacement X').position(180, height);
-
-  dspy = createSlider(0, 0.2, 0, 0.01);
-  dspy.position(180, height + 100);
-  createP('Displacement Y').position(180, height+60);
-
-  fct = createSlider(0, 300, 100, 1);
-  fct.position(180, height + 160);
-  createP('Offset').position(180, height+120);
-
-
 }
 
 function windowResized() {
